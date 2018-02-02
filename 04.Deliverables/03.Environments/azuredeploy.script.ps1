eef4075ebb89c545a0ed303e8e924913eaa31fb8
Sky Sigal <sky.sigal@ministryof.me>
2018-02-02 22:35:17 +1300

    X

--- 04.Deliverables/03.Environments/azuredeploy.script.ps1
+++ 04.Deliverables/03.Environments/azuredeploy.script.ps1
@@ -12,12 +12,22 @@
 
 # Context:
 BUILD_ARTIFACTSTAGINGDIRECTORY
+
+# https://docs.microsoft.com/en-gb/vsts/build-release/concepts/definitions/release/variables?tabs=batch#default-variables
+
+System.TeamProjectId
+Write-Host "...System.TeamProject: $(System_TeamProject)"
+Write-Host "...System.TeamProjectId: $(System_TeamProjectId)"
+
+Write-Host "...."
+
 Write-Host "...BUILD_ARTIFACTSTAGINGDIRECTORY: $(BUILD_ARTIFACTSTAGINGDIRECTORY)"
 Write-Host "...BUILD_BUILDID: $(BUILD_BUILDID)"
 Write-Host "...BUILD_BUILDNUMBER: $(BUILD_BUILDNUMBER)"
 Write-Host "...BUILD_SOURCEDIRECTORY: $(BUILD_SOURCEDIRECTORY)"
-Write-Host "...BUILD_STAGINGDIRECTORY: $(BUILD_STAGINGDIRECTORY)"
+Write-Host "...BUIL_STAGINGDIRECTORY: $(BUILD_STAGINGDIRECTORY)"
 Write-Host "...BUILD_BINARIESDIRECTORY: $(BUILD_BINARIESDIRECTORY)"
+Write-Host "...BUILD_WORKINGDIRECTORY: $(BUILD_WORKINGDIRECTORY)"
 Write-Host "...BUILD_REASON: $(BUILD_REASON)"
 Write-Host "...BUILD_REPOSITORY_CLEAN: $(BUILD_REPOSITORY_CLEAN)"
 Write-Host "...BUILD_SOURCEBRANCH: $(BUILD_SOURCEBRANCH)"
@@ -36,8 +46,14 @@ Write-Host "...BUILD_REPOSITORY_CLEAN: $(ENV:BUILD_REPOSITORY_CLEAN)"
 Write-Host "...BUILD_SOURCEBRANCH: $(ENV:BUILD_SOURCEBRANCH)"
 Write-Host "...BUILD_SOURCEBRANCH_NAME: $(ENV:BUILD_SOURCEBRANCH_NAME)"
 
+# Create Temp Vars:
 
 # Cleanup Parameters:
+# ...Branch Name:
+$buildSourceBranchName = $ENV:BUILD_SOURCEBRANCH_NAME;
+if ($buildSourceBranchName -eq $null){$buildSourceBranchName = "";}
+if ($buildSourceBranchName -eq "master"){$buildSourceBranchName = "";}
+
 # ...Cleanup the ARM Location Uri/Path:
 $defaultArmLocation = $env:custom_vars_DEFAULTARMLOCATION;
 if ($defaultArmLocation -eq $null){$defaultArmLocation = "";}
@@ -56,8 +72,14 @@ Write-Host "...resourceNameTemplate: $(resourceNameTemplate)"
 # Create Name of ResourceGroup
 Write-Host "Solving Resouce Group Name Template"
 Write-Host "...resourceNameTemplate: $(resourceNameTemplate)"
-Write-Host "...Replaceing '{APPINST}' found within 'env:CUSTOM_VARS_RESOURCENAMETEMPLATE':"
-$resourceNameTemplate= $resourceNameTemplate.Replace("{APPINST}", $appInstanceIdentifier);
+Write-Host "...Replaceing '{ENV}'/'{ENVID}'/'{ENVIDENTIFIER} found within 'env:CUSTOM_VARS_RESOURCENAMETEMPLATE':"
+$resourceNameTemplate= $resourceNameTemplate.Replace("{ENVIDENTIFIER}", $targetEnvironmentIdentifier);
+$resourceNameTemplate= $resourceNameTemplate.Replace("{ENVID}", $targetEnvironmentIdentifier);
+$resourceNameTemplate= $resourceNameTemplate.Replace("{ENV}", $targetEnvironmentIdentifier);
+$resourceNameTemplate= $resourceNameTemplate.Replace("{SOURCEBRANCHNAME}", $buildSourceBranchName);
+$resourceNameTemplate= $resourceNameTemplate.Replace("{SOURCEBRANCH}", $buildSourceBranchName);
+$resourceNameTemplate= $resourceNameTemplate.Replace("{BRANCHNAME}", $buildSourceBranchName);
+$resourceNameTemplate= $resourceNameTemplate.Replace("{BRANCH}", $buildSourceBranchName);
 $resourceNameTemplate= $resourceNameTemplate.Replace("--", "-");
 Write-Host "...resourceNameTemplate: $(resourceNameTemplate)"
 
