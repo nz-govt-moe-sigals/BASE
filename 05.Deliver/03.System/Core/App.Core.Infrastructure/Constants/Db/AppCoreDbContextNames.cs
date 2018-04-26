54f7e78c6748973baaf1e3721063ff03dd031c55
Sky Sigal <sky.sigal@ministryof.me>
2018-04-26 17:23:08 +1200

    Changed connectionstring Name to "Default" to line up with ARM templates.

--- 05.Deliver/03.System/Core/App.Core.Infrastructure/Constants/Db/AppCoreDbContextNames.cs
+++ 05.Deliver/03.System/Core/App.Core.Infrastructure/Constants/Db/AppCoreDbContextNames.cs
@@ -4,7 +4,7 @@
 
     public static class AppCoreDbConnectionStringNames
     {
-        public const string App = "AppCoreDbContext";
+        public const string Default = "Default";
     }
 
     public static class AppCoreDbContextNames
