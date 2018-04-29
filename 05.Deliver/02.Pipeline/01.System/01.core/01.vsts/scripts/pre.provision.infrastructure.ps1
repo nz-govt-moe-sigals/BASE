f9c8641527bd3073696e9b3316089bb37a25d1eb
Sky Sigal <sky.sigal@ministryof.me>
2018-04-29 17:34:33 +1200

    Added pre/post provision.infrastructure scripts.

---
+++ 05.Deliver/02.Pipeline/01.System/01.core/01.vsts/scripts/pre.provision.infrastructure.ps1
@@ -0,0 +1,222 @@
+
+[cmdletbinding]
+# Define method before invoking it
+function Provision-Variables {
+    <#
+    .SYNOPSIS
+    ...etc...
+    .DESCRIPTION
+    ...etc...
+    .EXAMPLE
+    Provision-AzureAD
+    .EXAMPLE
+    Provision-AzureAD -adDomainName:myschool.school.nz
+    .PARAMETER adDomainName
+    ...etc...
+    .PARAMETER adPrincipalName
+    ...etc...
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(mandatory = $false)] [string]$defaultMasterBranchNameReplacement = "0000",
+        [Parameter(mandatory = $false)] [string]$defaultResourceLocationIdentifier = "australiaeast"
+
+    )
+
+    
+    <#
+  # README
+  This script 
+
+  ## BACKGROUND
+  Visual Studio Online (VSO)'s easy, Drag/Drop system to build a Build Definitions has the advantage
+  of being easy to drag/drop and put together basic recipes, with a GUI to help determine the
+  input parameters -- but it's also at the cost of keeping the recipe version controlled along
+  with the deliverable code (don't forget the deliverable is the pipeline that delivers product, 
+  not the product itself).
+  Since we're developers...writting code is what we do. Including Build Pipelines. 
+
+  The script is heavily annotated to make it more accessible to team members of varying Powershell ability.
+
+  ### PREREQUISITES: VARIABLES
+  The script is expecting the following Custom Variable Inputs being set in the Build Definition Variables:
+  # OPTIONAL: CUSTOM_COMMON_VARS_ORGIDENTIFIER (eg: 'NZ-MOE')
+  # OPTIONAL: CUSTOM_COMMON_VARS_APPIDENTIFIER (eg: 'FOO')
+  # OPTIONAL: CUSTOM_VARS_MASTERBRANCHNAMEREPLACEMENT (eg: '0000')
+  # OPTIONAL: CUSTOM_VARS_DEFAULTRESOURCELOCATION (eg: 'australiaeast')
+  # OPTIONAL: CUSTOM_VARS_ENVIDENTIFIER (eg: BT, DT, ST, UAT, PROD, etc.)
+  # REQUIRED: CUSTOM_VARS_RESOURCENAMETEMPLATE (eg: 'MYORG-MYAPP-{BRANCHNAME}-{ENVID}-{RT}')
+
+
+  ### PREREQUISITE KNOWLEDGE: POWERSHELL
+  * Powershell is powerful while remaining easy. That said, a quick primer never hurt:
+  * http://skysigal.com/it/ad/powershell/howto/syntax_basics/home 
+
+  ### PREREQUISITE KNOWLEDGE: HOW TO GET CONTEXT INFO WITHIN YOUR SCRIPTS.
+  Essentially, by the time this script is invoked, VSO will have pumped into a ton of variables.
+  * More info: https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=batch#predefined-variables
+  * There is an Agent object
+  * There is also a Server object
+  * There is also a Build object
+  * Every Custom Variable (eg: 'CUSTOM_VARS_Foo') you create in a VSO Build Definition will 
+    be pumped into this memory space as $ENV:CUSTOM_VARS_FOO (note how it is an Env var, and the dots
+    are replaced with '_')
+  * you can also pass arguments to the script when you invoke it.
+
+  ### PREREQUISITE KNOWLEDGE: AZURE AUTHENTICATION/SUBSCRIPTION CONTEXT
+  Variables are then used to create or privision Resources within a specific target Azure Subscription. 
+
+  ### PREREQUISITES KNOWLEDGE: ARMS
+  You can find ARM Templates to look at here: https://azure.microsoft.com/en-us/resources/templates/?sort=Popular
+
+
+  ### RESOURCES
+  * https://github.com/Microsoft/vsts-tasks/blob/master/docs/authoring/commands.md
+  * https://docs.microsoft.com/en-gb/vsts/build-release/concepts/definitions/release/variables?tabs=batch#default-variables
+  #>
+
+
+  # Cleanup Variables, Parameters and make local parameters:
+
+
+    # Get the Org Id (eg: 'NZ-MOE')
+    $orgIdentifier = $ENV:CUSTOM_COMMON_VARS_ORGANISATIONIDENTIFIER
+    if ([string]::IsNullOrEmpty($orgIdentifier)) {$orgIdentifier = $ENV:CUSTOM_COMMON_VARS_ORGIDENTIFIER; }
+    if ([string]::IsNullOrEmpty($orgIdentifier)) {$orgIdentifier = $ENV:CUSTOM_VARS_ORGANISATIONIDENTIFIER; }
+    if ([string]::IsNullOrEmpty($orgIdentifier)) {$orgIdentifier = $ENV:CUSTOM_VARS_ORGIDENTIFIER; }
+    if ([string]::IsNullOrEmpty($orgIdentifier)) {$orgIdentifier = ""; }
+
+    # Get the App Id (eg: 'MYAPP')
+    $appIdentifier = $ENV:CUSTOM_COMMON_VARS_APPIDENTIFIER
+    if ([string]::IsNullOrEmpty($appIdentifier)) {$appIdentifier = $ENV:CUSTOM_COMMON_VARS_SYSTEMIDENTIFIER; }
+    if ([string]::IsNullOrEmpty($appIdentifier)) {$appIdentifier = $ENV:CUSTOM_VARS_APPIDENTIFIER; }
+    if ([string]::IsNullOrEmpty($appIdentifier)) {$appIdentifier = $ENV:CUSTOM_VARS_SYSTEMIDENTIFIER; }
+    if ([string]::IsNullOrEmpty($appIdentifier)) {$appIdentifier = ""; }
+
+    # replace 'master' with '', '0000' ... or even 'master'.
+    $masterBranchNameReplacement = $ENV:CUSTOM_COMMON_VARS_MASTERBRANCHNAMEREPLACEMENT
+    if ([string]::IsNullOrEmpty($masterBranchNameReplacement)) {$masterBranchNameReplacement = $ENV:CUSTOM_VARS_MASTERBRANCHNAMEREPLACEMENT; }
+    if ([string]::IsNullOrEmpty($masterBranchNameReplacement)) {$masterBranchNameReplacement = $defaultMasterBranchNameReplacment; }
+
+    # the git branch name should be a Userstory:
+    $buildSourceBranchName = $ENV:BUILD_SOURCEBRANCH_NAME;
+    if ([string]::IsNullOrEmpty($buildSourceBranchName)) {$buildSourceBranchName = ""; }
+    if ($buildSourceBranchName -eq "master") {$buildSourceBranchName = $masterBranchNameReplacement; }
+
+    # EnvIdentifier is going to be something like BT, DT, ST, UAT, PROD, etc.
+    $envIdentifier = $env:CUSTOM_VARS_ENVIDENTIFIER;
+    if ([string]::IsNullOrEmpty($envIdentifier)) {$envIdentifier = ""; }
+
+    # resourceNameTemplate is going to be something like MYORG-MYAPP-{BRANCHNAME}-{ENVID}-{RT}
+    $resourceNameTemplate = $env:CUSTOM_VARS_RESOURCENAMETEMPLATE;
+    if ([string]::IsNullOrWhiteSpace($resourceNameTemplate)) {$resourceNameTemplate = ""; }
+
+    # default location should be 'australiaeast':
+    $defaultResourceLocation = $env:CUSTOM_COMMON_VARS_DEFAULTRESOURCELOCATION;
+    if ([string]::IsNullOrEmpty($defaultResourceLocation)) {$defaultResourceLocation = $env:CUSTOM_VARS_DEFAULTRESOURCELOCATION; }
+    if ([string]::IsNullOrEmpty($defaultResourceLocation)) {$defaultResourceLocation = $defaultResourceLocationIdentifier; }
+
+
+    # Output System, Build's default and injected Variables:
+    Write-Host "Script variables of potential interest:"
+    Write-Host "...PSScriptRoot: $PSScriptRoot"
+
+    Write-Host "Agent Variable of potential interests at some point:"
+    Write-Host "...Agent.Id: $ENV:AGENT_ID"
+    Write-Host "...Agent.MachineName: $ENV:AGENT_MACHINENAME"
+    Write-Host "...Agent.BuildDirectory: $ENV:AGENT_BUILDDIRECTORY"
+    Write-Host "...Agent.WorkFolder: $ENV:AGENT_WORKFOLDER"
+    Write-Host "...Agent.JobStatus: $ENV:AGENT_JOBSTATUS"
+
+    Write-Host "System Variable of potential interests:"
+    Write-Host "...System.TeamProject: $ENV:SYSTEM_TEAMPROJECT"
+    Write-Host "...System.TeamProjectId: $ENV:SYSTEM_TEAMPROJECTID"
+
+    Write-Host "Build Variables of potential interest:"
+    Write-Host "...BUILD_BUILDID: $ENV:BUILD_BUILDID"
+    Write-Host "...BUILD_BUILDNUMBER: $ENV:BUILD_BUILDNUMBER"
+    Write-Host "...BUILD_SOURCEDIRECTORY: $ENV:BUILD_SOURCEDIRECTORY"
+    Write-Host "...BUILD_STAGINGDIRECTORY: $ENV:BUILD_STAGINGDIRECTORY"
+    Write-Host "...BUILD_ARTIFACTSTAGINGDIRECTORY: $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY"
+    Write-Host "...BUILD_BINARIESDIRECTORY: $ENV:BUILD_BINARIESDIRECTORY"
+    Write-Host "...BUILD_WORKINGDIRECTORY: $ENV:BUILD_WORKINGDIRECTORY"
+    Write-Host "...BUILD_REASON: $ENV:BUILD_REASON"
+    Write-Host "...BUILD_REPOSITORY_CLEAN: $ENV:BUILD_REPOSITORY_CLEAN"
+    Write-Host "...BUILD_SOURCEBRANCH: $ENV:BUILD_SOURCEBRANCH"
+    Write-Host "...BUILD_SOURCEBRANCHNAME: $ENV:BUILD_SOURCEBRANCHNAME"
+
+    Write-Host ""
+
+    # Create Name of ResourceGroup
+    Write-Host "Solving Resource Group Name Template"
+    Write-Host "...resourceNameTemplate (as received): $resourceNameTemplate"
+
+    Write-Host "...Replacing {ORG[ID][ENTIFIER]} within 'env:CUSTOM_VARS_RESOURCENAMETEMPLATE':"
+    $resourceNameTemplate = $resourceNameTemplate `
+        -replace "{ORGANISATIONIDENTIFIER}", $orgIdentifier `
+        -replace "{ORGIDENTIFIER}", $orgIdentifier `
+        -replace "{ORGID}", $orgIdentifier `
+        -replace "{ORG}", $orgIdentifier
+
+    Write-Host "...Replacing {APP[LICATION][ID][ENTIFIER]} within 'env:CUSTOM_VARS_RESOURCENAMETEMPLATE':"
+    $resourceNameTemplate = $resourceNameTemplate `
+        -replace "{APPLICATIONIDENTIFIER}", $appIdentifier `
+        -replace "{APPIDENTIFIER}", $appIdentifier `
+        -replace "{APPID}", $appIdentifier `
+        -replace "{APP}", $appIdentifier
+    
+    Write-Host "...Replacing {ENV[ID][ENTIFIER]} within 'env:CUSTOM_VARS_RESOURCENAMETEMPLATE':"
+    $resourceNameTemplate = $resourceNameTemplate `
+        -replace "{ENVIDENTIFIER}", $envIdentifier `
+        -replace "{ENVID}", $envIdentifier `
+        -replace "{ENV}", $envIdentifier
+
+    Write-Host "...Replacing '{[SOURCE]BRANCH[NAME|ID[DENTIFIER]]}' within 'env:CUSTOM_VARS_RESOURCENAMETEMPLATE':"
+    $resourceNameTemplate = $resourceNameTemplate `
+        -replace "{SOURCEBRANCHIDENTIFIER}", $buildSourceBranchName `
+        -replace "{SOURCEBRANCHID}", $buildSourceBranchName `
+        -replace "{BRANCHIDENTIFIER}", $buildSourceBranchName `
+        -replace "{BRANCHID}", $buildSourceBranchName `
+        -replace "{SOURCEBRANCHID}", $buildSourceBranchName `
+        -replace "{SOURCEBRANCHNAME}", $buildSourceBranchName `
+        -replace "{SOURCEBRANCH}", $buildSourceBranchName `
+        -replace "{BRANCHNAME}", $buildSourceBranchName `
+        -replace "{BRANCH}", $buildSourceBranchName
+
+    # Remove final dashes and duplicates:
+    $resourceNameTemplate = $resourceNameTemplate -replace "--", "-"
+    
+    # $resourceNameTemplate= [Regex]::Replace($resourceNameTemplate.Replace("--", "-"),"(.*)-*$","$1");
+    Write-Host "...resourceNameTemplate (cleaned up): $resourceNameTemplate"
+
+    $resourceGroupName = $resourceGroupName `
+        -replace "{RESOURCETYPE}", "" `
+        -replace "{RT}", "" `
+        -replace "-", ""
+
+    
+    Write-Host "...ResourceGroupName set to $resourceGroupName"
+
+    # Now that ResourceGroup name is created, safe to lowercase the resourcenametemplate:
+    $resourceNameTemplate = $resourceNameTemplate.ToLower()
+
+    # Saving the updated variables in the globally ENV space, 
+    # TIP:
+    # Know that Setting Variables does not update the Global till the end of this Task, before the next Task.
+    # ie, they consider them as Task Outputs.
+    Write-Host "Updating Global Env Variables for next tasks..."
+    # TIP: You have to name the vars as they were registered in vsts (with dots, not dashes)
+    Write-Host "##vso[task.setvariable variable=CUSTOM_VARS_ENVIDENTIFIER;]$envIdentifier"
+    Write-Host "##vso[task.setvariable variable=CUSTOM_VARS_RESOURCEGROUPNAME;]$resourceGroupName"
+    Write-Host "##vso[task.setvariable variable=CUSTOM_VARS_RESOURCENAMETEMPLATE;]$resourceNameTemplate"
+    Write-Host "##vso[task.setvariable variable=CUSTOM_VARS_DEFAULTRESOURCELOCATION;]$defaultResourceLocation"
+
+    Write-Host "Task Complete."  
+}
+
+# --------------------------------------------------
+# --------------------------------------------------
+# Invoke Method
+Provision-Variables -defaultMasterBranchNameReplacement:"0000" -defaultResourceLocationIdentifier:"australiaeast"
+# --------------------------------------------------
+# --------------------------------------------------
