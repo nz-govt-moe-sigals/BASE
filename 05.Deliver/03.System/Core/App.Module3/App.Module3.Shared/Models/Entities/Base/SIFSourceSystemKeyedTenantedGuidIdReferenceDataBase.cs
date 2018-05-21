ae8797ae975d219a6a0f1af7c5fabebe4d2bd4f5
Sky Sigal <sky.sigal@ministryof.me>
2018-05-21 16:12:41 +1200

    Moved Host and Module3 into subfolders.

---
+++ 05.Deliver/03.System/Core/App.Module3/App.Module3.Shared/Models/Entities/Base/SIFSourceSystemKeyedTenantedGuidIdReferenceDataBase.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace App.Module3.Shared.Models.Entities
+{
+    using App.Core.Shared.Models.Entities.Base;
+
+    /// <summary>
+    /// Abstract base class 
+    /// for System ReferenceData entities.
+    /// <para>
+    /// Key aspects that are common to all ReferenceData
+    /// include 
+    /// * the Source Id is persisted as the <see cref="Models.IHasSourceSystemKey.SourceSystemKey"/>
+    /// * the Source Id is used to find and fill in the <see cref="IHasSIFKey.SIFKey"/> value.
+    /// </para>
+    /// </summary>
+    /// <seealso cref="App.Core.Shared.Models.Entities.Base.TenantFKAuditedRecordStatedTimestampedGuidIdReferenceDataEntityBase" />
+    /// <seealso cref="App.Module3.Shared.Models.IHasSourceSystemKey" />
+    /// <seealso cref="App.Module3.Shared.Models.IHasSIFKey" />
+    // ReSharper disable once InconsistentNaming
+    public class SIFSourceSystemKeyedTenantedGuidIdReferenceDataBase : SourceSystemKeyedTenantedGuidIdReferenceDataBase, IHasSIFKey
+    {
+ 
+
+        /// <summary>
+        /// Gets or sets the PK used by SIF API Messages.
+        /// <para>
+        /// The PK value is not always defined in the SIF.
+        /// It can fall back to MOE values, which in turn
+        /// are falling back to NZSTATS values.
+        /// </para><para>
+        /// The Type is String as the value
+        /// can be a character (eg: Gender),
+        /// a word, or an integer
+        /// </para>
+        /// </summary>
+        public string SIFKey { get; set; }
+    }
+}
---
+++ 05.Deliver/03.System/Core/App.Module3/App.Module3.Shared/Models/Entities/EducationProviderLocation.cs
@@ -0,0 +1,54 @@
+﻿namespace App.Module3.Shared.Models.Entities
+{
+    using System;
+    using App.Core.Shared.Models.Entities;
+    using App.Module3.Shared.Models.Enums;
+
+    public class EducationProviderLocation : TenantFKAuditedRecordStatedTimestampedGuidIdEntityBase, IHasSourceSystemKey
+    {
+
+        public Guid EducationProviderFK { get; set; }
+
+
+        public virtual LocationType Type
+        {
+            get; set;
+        }
+
+
+
+        //public virtual Guid OwnerFK { get; set; }        
+        /// <summary>
+        /// Gets or sets the latitude (the 0 to 90 North or South).
+        /// </summary>
+        public virtual decimal Latitude
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the longitude (the 0 to +/-180).
+        /// </summary>
+        public virtual decimal Longitude
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the altitude.
+        /// </summary>
+        public virtual decimal? Altitude
+        {
+            get; set;
+        }
+
+
+        /// <summary>
+        /// The Reference(record) Id that was received from the source 
+        /// </summary>
+        public string SourceSystemKey { get; set; }
+
+
+        public string SourceSystemName { get; set; }
+    }
+}
