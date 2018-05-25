f0eda681c31f65f1b5a39892d8f64033e8e3e717
Peter w <peter.williamson@equinox.co.nz>
2018-05-25 01:15:30 +0000

    maintainability update

--- 05.Deliver/03.System/Core/App.Module22/App.Module22.Infrastructure/Initialization/ObjectMaps/Messages/V0100/ObjectMap_Course_CourseDto.cs
+++ 05.Deliver/03.System/Core/App.Module22/App.Module22.Infrastructure/Initialization/ObjectMaps/Messages/V0100/ObjectMap_Course_CourseDto.cs
@@ -1,12 +1,11 @@
-﻿namespace App.Module01.Infrastructure.Initialization.ObjectMaps.Messages.V0100
-{
-    using App.Core.Infrastructure.Initialization;
-    using App.Core.Infrastructure.Initialization.ObjectMaps;
-    using App.Core.Shared.Models.Messages.API.V0100;
-    using App.Module22.Shared.Models.Entities;
-    using AutoMapper;
+﻿using App.Core.Infrastructure.Initialization.ObjectMaps;
+using App.Core.Shared.Models.Messages.API.V0100;
+using App.Module22.Shared.Models.Entities;
+using AutoMapper;
 
-    public class ObjectMap_Example_ExampleDto : IHasAutomapperInitializer
+namespace App.Module22.Infrastructure.Initialization.ObjectMaps.Messages.V0100
+{
+    public class ObjectMap_Course_CourseDto : IHasAutomapperInitializer
     {
         public void Initialize(IMapperConfigurationExpression config)
         {
