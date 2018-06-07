246bff3de7a0442e6e46656aded806b0f1228025
Peter w <peter.williamson@equinox.co.nz>
2018-06-07 02:12:32 +0000

    still more wip

--- 05.Deliver/03.System/Core/App.Module32/App.Module32.Application/API/Controllers/V0100/TransportController.cs
+++ 05.Deliver/03.System/Core/App.Module32/App.Module32.Application/API/Controllers/V0100/TransportController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using App.Core.Application.API.Controllers.Base.Base;
 using App.Core.Infrastructure.Services;
-using App.Module31.Infrastructure.Services;
+using App.Module32.Infrastructure.Services;
 using App.Module32.Shared.Models.Messages.API.V0100;
 
 namespace App.Module32.Application.API.Controllers.V0100
@@ -31,11 +31,11 @@ namespace App.Module32.Application.API.Controllers.V0100
 
         [AllowAnonymous]
         [HttpGet]
-        public string Extract()
+        public async Task<string> ExtractAsync()
         {
             using (var elapsedTime = new ElapsedTime())
             {
-                _extractServiceController.ProcessAllTables();
+                await _extractServiceController.ProcessAllTablesAsync();
 
                 return $"success - elapsedTime : {elapsedTime.ElapsedText}";
             }
