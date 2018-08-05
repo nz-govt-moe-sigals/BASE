ae160a50cfe7cf887f3eb6e4502d19742fe29386
Peter w <peter.williamson@equinox.co.nz>
2018-08-05 22:51:39 +0000

    cyclo

--- 05.Deliver/03.System/Core/App.Core/App.Core.Infrastructure/Initialization/ObjectMaps/Messages/V0100/ObjectMap_MediaMetadata_MediaMetadataDto.cs
+++ 05.Deliver/03.System/Core/App.Core/App.Core.Infrastructure/Initialization/ObjectMaps/Messages/V0100/ObjectMap_MediaMetadata_MediaMetadataDto.cs
@@ -45,18 +45,26 @@ namespace App.Core.Infrastructure.Initialization.ObjectMaps.Messages.V0100
                 .ForMember(t => t.ContentSize, opt => opt.MapFrom(s => s.ContentSize))
                 .ForMember(t => t.ContentHash, opt => opt.MapFrom(s => s.ContentHash))
                 .ForMember(t => t.LocalName, opt => opt.MapFrom(s => s.LocalName))
-                .ForMember(t => t.LatestScanDateTimeUtc, opt => opt.MapFrom(s => s.LatestScanDateTimeUtc))
-                .ForMember(t => t.LatestScanMalwareDetetected, opt => opt.MapFrom(s => s.LatestScanMalwareDetetected))
-                .ForMember(t => t.LatestScanResults, opt => opt.MapFrom(s => s.LatestScanResults))
+               
                 .ForMember(t => t.MimeType, opt => opt.MapFrom(s => s.MimeType))
                 .ForMember(t => t.SourceFileName, opt => opt.MapFrom(s => s.SourceFileName))
                 .ForMember(t => t.TenantFK, opt => opt.MapFrom(s => s.TenantFK))
                 .ForMember(t => t.UploadedDateTimeUtc, opt => opt.MapFrom(s => s.UploadedDateTimeUtc))
                 .ForMember(t => t.DataClassificationFK, opt => opt.Ignore())
                 ;
+            MapLatest(mappingExpression);
             MapBase(mappingExpression);
             // Use an Enum as DataClassification is shared across Bounded DbContexts
             // Results of scanning, whenever done:
+        }
+
+        private void MapLatest(IMappingExpression<MediaMetadataDto, MediaMetadata> mappingExpression)
+        {
+            mappingExpression
+                .ForMember(t => t.LatestScanDateTimeUtc, opt => opt.MapFrom(s => s.LatestScanDateTimeUtc))
+                .ForMember(t => t.LatestScanMalwareDetetected, opt => opt.MapFrom(s => s.LatestScanMalwareDetetected))
+                .ForMember(t => t.LatestScanResults, opt => opt.MapFrom(s => s.LatestScanResults))
+                ;
         }
     }
 }
